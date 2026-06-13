141f3819c6e5a3d46ed5b929e1986c8522e8e44c
Ferdinand <atiferdinand31@gmail.com>
2026-06-13 23:09:31 +0100

    feat: add print3 task to demonstrate mutex functionality

--- examples/mutex_and_print/main.cpp
+++ examples/mutex_and_print/main.cpp
@@ -22,6 +22,19 @@ void print1(void *arg) {
     }    
 }
 
+void print3(void *arg) {
+    const char *str = "[TASK 3] --- Japan is talking ---\r\n";
+    while (1) {
+        mutex.lock();
+        for (int i = 0; str[i] != '\0'; i++) {
+            uart_putc_raw(uart0, str[i]);
+            sleep_ms(100);
+        }
+        mutex.unlock();
+        vecos::sleep_task_ms(500); // Laisse un vrai temps d'observation de 500ms !
+    }    
+}
+
 void print2(void *arg) {
     const char *str = "[TASK 2] *** China is talking ***\r\n";
     while (1) {
@@ -39,6 +52,7 @@ void print2(void *arg) {
 
 vecos::Task<1024> task1(print1);
 vecos::Task<1024> task2(print2);
+vecos::Task<1024> task3(print3);
 
 vecos::Scheduler os;
 
@@ -46,6 +60,7 @@ int main() {
     stdio_init_all();
     os.add_task(task1);
     os.add_task(task2);
+    os.add_task(task3);
     os.start();
     while (1)
     {
